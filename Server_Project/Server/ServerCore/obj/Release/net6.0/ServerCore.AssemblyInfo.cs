47d54a386f8e7c8e12d6374b5c1f46ebf59fd1ac
reahyun <narh0302@gmail.com>
2024-09-07 01:45:06 +0900

    Thread 관련 최적화 문제

--- Server_Project/Server/ServerCore/obj/Release/net6.0/ServerCore.AssemblyInfo.cs
+++ Server_Project/Server/ServerCore/obj/Release/net6.0/ServerCore.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerCore")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf38d60a8b7b63e9dec74e69193d7fdd80afa66c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerCore")]
