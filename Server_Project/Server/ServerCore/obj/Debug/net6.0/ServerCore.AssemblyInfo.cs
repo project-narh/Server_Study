cf38d60a8b7b63e9dec74e69193d7fdd80afa66c
reahyun <narh0302@gmail.com>
2024-09-07 01:11:43 +0900

    Thread 사용법 및 설명

--- Server_Project/Server/ServerCore/obj/Debug/net6.0/ServerCore.AssemblyInfo.cs
+++ Server_Project/Server/ServerCore/obj/Debug/net6.0/ServerCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1790eef1582bc0883e8305c512f60432e0cbd35b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8201cdc035723b12be979b77f9f83de21d10d684")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
