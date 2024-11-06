67bf622914f5e9f98794ef5dd6b5580409268e75
reahyun <narh0302@gmail.com>
2024-11-06 20:37:45 +0900

    15. TCp vs UDP

--- Server_Project/Server/ServerCore/obj/Release/net6.0/ServerCore.AssemblyInfo.cs
+++ Server_Project/Server/ServerCore/obj/Release/net6.0/ServerCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b982df0d073ba794b918dcbd6feacc70128e5511")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+631b4e241c117dbab8d2a4ed5ea0d6ca937f0ef1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
